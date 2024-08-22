f8ec23c36c21e4bb2fd20bd4a48ba130856e1758
SchneiderMaster <fynn2746@outlook.de>
2024-08-22 16:40:11 +0200

    modified the maintileservice to open the activity

--- app/src/main/java/com/schneidermaster/selvecontrol/tile/MainTileService.java
+++ app/src/main/java/com/schneidermaster/selvecontrol/tile/MainTileService.java
@@ -1,11 +1,15 @@
 package com.schneidermaster.selvecontrol.tile;
 
+import android.content.ComponentName;
+import android.content.Intent;
+
 import androidx.annotation.NonNull;
-import androidx.wear.protolayout.ColorBuilders;
+import androidx.wear.protolayout.ActionBuilders;
+import androidx.wear.protolayout.LayoutElementBuilders;
+import androidx.wear.protolayout.ModifiersBuilders;
 import androidx.wear.protolayout.ResourceBuilders;
 import androidx.wear.protolayout.TimelineBuilders;
 import androidx.wear.protolayout.material.Text;
-import androidx.wear.protolayout.material.Typography;
 import androidx.wear.tiles.RequestBuilders;
 import androidx.wear.tiles.TileBuilders;
 import androidx.wear.tiles.TileService;
@@ -20,16 +24,36 @@ public class MainTileService extends TileService {
     @NonNull
     @Override
     protected ListenableFuture<TileBuilders.Tile> onTileRequest(@NonNull RequestBuilders.TileRequest requestParams) {
+
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setComponent(new ComponentName(getPackageName(), "com.schneidermaster.selvecontrol.presentation.MainActivity"));
+
+        LayoutElementBuilders.Box box = new LayoutElementBuilders.Box.Builder()
+                .addContent(new Text.Builder(this, "Tap me").build())
+                .setModifiers(new ModifiersBuilders.Modifiers.Builder()
+                        .setClickable(new ModifiersBuilders.Clickable.Builder()
+                                .setOnClick(new ActionBuilders.LaunchAction.Builder()
+                                        .setAndroidActivity(new ActionBuilders.AndroidActivity.Builder()
+                                                .setPackageName(getPackageName())
+                                                .setClassName("com.schneidermaster.selvecontrol.presentation.MainActivity")
+                                                .build()
+                                        )
+                                        .build()
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .build();
+
         return Futures.immediateFuture(new TileBuilders.Tile.Builder()
                 .setResourcesVersion(RESOURCES_VERSION)
                 .setTileTimeline(
                         TimelineBuilders.Timeline.fromLayoutElement(
-                                new Text.Builder(this, "Hello world!")
-                                        .setTypography(Typography.TYPOGRAPHY_DISPLAY1)
-                                        .setColor(ColorBuilders.argb(0xFFFF0000))
-                                        .build()))
-                .build()
-        );
+                                box
+                        )).build());
     }
 
     @NonNull
