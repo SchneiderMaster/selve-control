41b6125167df30b5f31640de243461914b269c19
SchneiderMaster <fynn2746@outlook.de>
2024-08-22 17:43:51 +0200

    started with some HTTP requests

--- app/src/main/java/com/schneidermaster/selvecontrol/presentation/MainActivity.kt
+++ app/src/main/java/com/schneidermaster/selvecontrol/presentation/MainActivity.kt
@@ -9,11 +9,14 @@ package com.schneidermaster.selvecontrol.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.Dimension.Companion.DP
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,13 +24,28 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import com.schneidermaster.selvecontrol.R
 import com.schneidermaster.selvecontrol.presentation.theme.SelveControlTheme
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -43,6 +61,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WearApp(greetingName: String) {
+
+    val client: OkHttpClient = OkHttpClient()
+
     SelveControlTheme {
         Box(
             modifier = Modifier
@@ -50,8 +71,34 @@ fun WearApp(greetingName: String) {
                 .background(MaterialTheme.colors.background),
             contentAlignment = Alignment.Center
         ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+            //TimeText()
+            Box(
+                modifier = Modifier
+            ){
+                ScalingLazyColumn {
+                    item {
+                        Button(
+                            onClick = {
+                                println("I have been pressed; " + "up")
+                                post(client, "http://192.168.188.143/cmd", "{\"XC_FNC\":\"SendGenericCmd\",\"id\":\"09\",\"data\":{\"cmd\": \"moveUp\"}}")
+                            })
+                        {
+                            Text(text = "Up")
+                        }
+                    }
+                    item {
+                        Button(
+                            onClick = {
+                                println("I have been pressed; " + "down")
+                                post(client, "http://192.168.188.143/cmd", "{\"XC_FNC\":\"SendGenericCmd\",\"id\":\"09\",\"data\":{\"cmd\": \"moveUp\"}}")
+                            })
+                        {
+                            Text(text = "Down")
+                        }
+                    }
+                }
+                
+            }
         }
     }
 }
@@ -70,4 +117,22 @@ fun Greeting(greetingName: String) {
 @Composable
 fun DefaultPreview() {
     WearApp("Preview Android")
+}
+
+fun post(client: OkHttpClient, url: String, json: String){
+
+    val thread = Thread(Runnable {
+        @Override
+        fun run(){
+            val body = json.toRequestBody("application/json".toMediaType())
+            val request = Request.Builder()
+                .url(url)
+                .post(body)
+                .build()
+
+            val response = client.newCall(request).execute()
+        }
+    })
+
+    thread.start()
 }
