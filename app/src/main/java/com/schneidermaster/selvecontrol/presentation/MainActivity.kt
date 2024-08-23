a4567409e39966c73cb37ac1b60dd083a7bd6091
SchneiderMaster <fynn2746@outlook.de>
2024-08-23 12:29:44 +0200

    added correct serialization, added stop button and got all shutter data

--- app/src/main/java/com/schneidermaster/selvecontrol/presentation/MainActivity.kt
+++ app/src/main/java/com/schneidermaster/selvecontrol/presentation/MainActivity.kt
@@ -11,29 +11,25 @@ package com.schneidermaster.selvecontrol.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
+import com.google.gson.JsonParser
 import com.schneidermaster.selvecontrol.presentation.theme.SelveControlTheme
 import com.schneidermaster.selvecontrol.tools.httprequests.get
 import com.schneidermaster.selvecontrol.tools.httprequests.post
-import com.schneidermaster.selvecontrol.tools.shutters.JsonData
-import com.schneidermaster.selvecontrol.tools.shutters.JsonResponse
 import com.schneidermaster.selvecontrol.tools.shutters.Shutter
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.OkHttpClient
 
 class MainActivity : ComponentActivity() {
@@ -87,6 +83,26 @@ fun WearApp() {
                             Text(text = "Up")
                         }
                     }
+
+                    item {
+                        Button(
+                            onClick = {
+
+                                println("I have been pressed; " + "stop")
+                                GlobalScope.launch {
+                                    val response = post(
+                                        client,
+                                        "http://192.168.188.143/cmd?auth=Auablume",
+                                        "{\"XC_FNC\":\"SendGenericCmd\",\"id\":\"0B\",\"data\":{\"cmd\": \"stop\"}}"
+                                    )
+                                    println(response.await().body?.string())
+                                }
+                            })
+                        {
+                            Text(text = "Stop")
+                        }
+                    }
+
                     item {
                         Button(
                             onClick = {
@@ -113,30 +129,55 @@ fun WearApp() {
 
 fun getShutters(client: OkHttpClient){
     GlobalScope.launch {
-        val deferredResponse = get(client, "http://192.168.188.143/cmd?XC_FNC=GetStates&auth=Auablume")
 
-        val response = deferredResponse.await()
-        val jsonResponse = response.body?.string()
+        val shutters: List<Shutter>
 
-        if(jsonResponse != null){
-            val json = Json { ignoreUnknownKeys = true }
-            val jsonResponseObject = json.decodeFromString<JsonResponse>(jsonResponse)
-            println(jsonResponseObject)
-            val devices = jsonResponseObject.XC_SUC
-                .mapNotNull { it as? JsonObject }
-                .filter{it["type"]?.jsonPrimitive?.content == "CM"}
-                .filterIsInstance<JsonData.Cm>()
-                .map{
+        val deferredStatesResponse = get(client, "http://192.168.188.143/cmd?XC_FNC=GetStates&auth=Auablume")
+
+        val statesResponse = deferredStatesResponse.await()
+        val statesJsonResponse = statesResponse.body?.string()
+
+        if(statesJsonResponse != null){
+            val jsonObject = JsonParser.parseString(statesJsonResponse).asJsonObject
+            val shuttersArray = jsonObject.getAsJsonArray("XC_SUC")
+
+            shutters = shuttersArray
+                .filter { it.asJsonObject.get("type")?.asString == "CM" }
+                .map {
+                    val obj = it.asJsonObject
                     Shutter(
-                        name = "",
-                        sid = it.sid,
-                        adr = it.adr,
-                        position = it.state?.position
+                        sid = obj.get("sid").asString,
+                        adr = obj.get("adr").asString,
+                        position = obj.get("state").asJsonObject.get("position").asInt,
+                        name = null
                     )
                 }
-
-            println(devices)
         }
+        else{
+            error("Didn't receive shutter data.")
+        }
+
+        shutters.forEach {
+            GlobalScope.launch {
+                val deferredConfigResponse = get(
+                    client,
+                    "http://192.168.188.143/cmd?XC_FNC=GetConfig&type=CM&adr=" + it.adr + "&auth=Auablume"
+                )
+                val configResponse = deferredConfigResponse.await()
+                val configJsonResponse = configResponse.body?.string()
+
+                if (configJsonResponse != null) {
+                    val jsonObject = JsonParser.parseString(configJsonResponse).asJsonObject
+                    it.name =
+                        jsonObject.get("XC_SUC").asJsonObject.get("info").asJsonObject.get("configurable").asJsonObject.get(
+                            "name"
+                        ).asString
+                }
+            }
+        }
+
+
+        println(shutters)
 
     }
 }
--- app/src/main/java/com/schneidermaster/selvecontrol/tools/shutters/Shutter.kt
+++ app/src/main/java/com/schneidermaster/selvecontrol/tools/shutters/Shutter.kt
@@ -1,35 +1,8 @@
 package com.schneidermaster.selvecontrol.tools.shutters
 
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Shutter (
-    val name: String?,
-    val adr: String?,
+data class Shutter(
     val sid: String?,
-    val position: Int?
+    val adr: String?,
+    val position: Int?,
+    var name: String?
 )
-
-@Serializable
-data class JsonResponse(
-    val XC_SUC: List<JsonData>
-)
-
-@Serializable
-sealed class JsonData {
-    @Serializable
-    data class Cm(
-        val sid: String?,
-        val adr: String?,
-        val cid: String?,
-        val state: State?
-    ) : JsonData()
-
-    @Serializable
-    data class State(
-        val position: Int?
-    )
-
-    @Serializable
-    data class Other(val type: String) : JsonData()
-}
